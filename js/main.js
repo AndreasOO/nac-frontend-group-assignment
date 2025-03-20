31ef74af0f3db1276da748fef9ac58a0055f510f
unknown <linn.edvardsson@yh.nackademin.se>
2025-03-20 15:05:14 +0100

    issue6

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("testing main script");
+class Item {
+    constructor(id, title, price, rating, description, image) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.rating = rating;
+        this.description = description;
+        this.image = image;
+    }
+}
+
+async function fetchProducts() {
+    return await fetch("https://fakestoreapi.com/products").then(result => result.json()).then(
+        json => json.map(productJsonObject => new Item(productJsonObject.id,
+                                                       productJsonObject.title,
+                                                       productJsonObject.price,
+                                                       productJsonObject.rating,
+                                                       productJsonObject.description,
+                                                       productJsonObject.image))
+    );
+}
+
+console.log(fetchProducts())
+
+
